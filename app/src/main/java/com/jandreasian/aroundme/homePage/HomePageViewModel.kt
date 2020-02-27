4fd4902edfe61d4e6f67201133eac55935f5093f
Josh <joshbarr7@gmail.com>
2020-02-26 22:49:34 -0800

    HomePage will now only load posts based on users location within a specific radius.

--- app/src/main/java/com/jandreasian/aroundme/homePage/HomePageViewModel.kt
+++ app/src/main/java/com/jandreasian/aroundme/homePage/HomePageViewModel.kt
@@ -5,20 +5,26 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.MetadataChanges
 import com.jandreasian.aroundme.network.LocationService
 import com.jandreasian.aroundme.network.Posts
+import org.imperiumlabs.geofirestore.GeoFirestore
+import org.imperiumlabs.geofirestore.listeners.GeoQueryEventListener
+import com.google.android.gms.location.LocationServices
+
 
 class HomePageViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val db = FirebaseFirestore.getInstance()
+    private val db = FirebaseFirestore.getInstance().collection("posts")
+    private val geoFirestore = GeoFirestore(db)
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(application)
 
     private val _posts = MutableLiveData<MutableList<Posts>>()
     val posts: MutableLiveData<MutableList<Posts>>
         get() = _posts
 
     private val locationData = LocationService(application)
-
 
     init {
         setUpListener()
@@ -29,35 +35,64 @@ class HomePageViewModel(application: Application) : AndroidViewModel(application
     /**
      * This will setup the snapshot listener to add/update any elements in the RecyclerView.
      */
-    fun setUpListener() {
+    private fun setUpListener() {
         var postList : MutableList<Posts> = mutableListOf()
-        val docRef = db.collection("posts")
-        docRef.addSnapshotListener(MetadataChanges.INCLUDE) { snapshot, e ->
+
+        fusedLocationClient!!.lastLocation
+            .addOnSuccessListener { location ->
+                //This will create a GeoQuery based on the current location with a radius of 0.1 kilometers.
+                val geoQuery = geoFirestore.queryAtLocation(GeoPoint(location.latitude, location.longitude), 0.1)
+
+                geoQuery.addGeoQueryEventListener(object : GeoQueryEventListener {
+                    override fun onGeoQueryError(exception: Exception) {
+                        Log.i("HomePageViewModel", "onGeoQueryError")
+                    }
+
+                    //This will add only posts that are within the area
+                    override fun onKeyEntered(documentID: String, location: GeoPoint) {
+                        db.document(documentID).get()
+                            .addOnSuccessListener { document ->
+                                if (document != null) {
+                                    Log.d("HomePageViewModel", "DocumentSnapshot data: ${document.get("caption")}")
+                                    var post = Posts(document.id, document.get("caption").toString(), document.get("imgSrcUrl").toString())
+                                    postList.add(post)
+                                } else {
+                                    Log.d("HomePageViewModel", "No such document")
+                                }
+                            }
+                            .addOnFailureListener { exception ->
+                            }
+                    }
+
+                    override fun onKeyMoved(documentID: String, location: GeoPoint) {
+                        Log.i("HomePageViewModel", "onKeyMoved")
+                    }
+
+                    override fun onKeyExited(key: String) {
+                        Log.i("HomePageViewModel", String.format("Provider %s is no longer in the search area", key))
+                    }
+
+                    override fun onGeoQueryReady() {
+                        Log.i("HomePageViewModel", "onGeoQueryReady")
+                        _posts.value = postList
+                    }
+                })
+            }
+
+        db.addSnapshotListener(MetadataChanges.INCLUDE) { snapshot, e ->
             if (e != null) {
                 Log.w("HomePageViewModel", "Listen failed.", e)
                 return@addSnapshotListener
             }
 
+            //This gets any updates that happen to the posts
             if (snapshot?.getDocumentChanges() != null) {
                 for (doc in snapshot!!.documentChanges) {
-                    var post = Posts(doc.document.id, doc.document.get("caption").toString(), doc.document.get("imgSrcUrl").toString())
-
-                    val a = postList.find { it.id == doc.document.id }
-                    if(a === null) {
-                        //Add new element in RecyclerView
-                        postList.add(post)
-
-                    } else {
-                        //Update existing element in RecyclerView
-                        postList?.find { it.id == doc.document.id }?.caption = doc.document.get("caption").toString()
-                    }
+                    postList?.find { it.id == doc.document.id }?.caption = doc.document.get("caption").toString()
                 }
-
-                _posts.value = postList
             } else {
                 Log.d("HomePageViewModel", "Current data: null")
             }
         }
-
     }
 }
